4606fc786f16025e56ee1673d255edfac1a08188
sldevops-store <sl.fiverr.devops@gmail.com>
2021-05-03 22:13:11 +0530

    buld folder added

---
+++ front-end/build/precache-manifest.fa5ede02406073bf9b8a17e3aef08d3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "310f18b15c38e5d8a2ff4ce38c3efcab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e926853b38ec418fea3",
+    "url": "/static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "63e99bebe9f15d4b68f6",
+    "url": "/static/css/main.b4bb0ec9.chunk.css"
+  },
+  {
+    "revision": "6e926853b38ec418fea3",
+    "url": "/static/js/2.03840490.chunk.js"
+  },
+  {
+    "revision": "2e69cb2b6cc335ccc96d4111650d8727",
+    "url": "/static/js/2.03840490.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63e99bebe9f15d4b68f6",
+    "url": "/static/js/main.7ba18252.chunk.js"
+  },
+  {
+    "revision": "b490e99b3af725a33edb",
+    "url": "/static/js/runtime-main.c0487561.js"
+  }
+]);
